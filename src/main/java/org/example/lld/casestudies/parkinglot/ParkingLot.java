d15ad2d10d5f2161a6ae2e5acfafe8a9f7c636ee
VineethNaidu <vineethnaidu.k@gmail.com>
2025-04-23 21:15:23 +0530

    "Low Level Design"

---
+++ src/main/java/org/example/lld/casestudies/parkinglot/ParkingLot.java
@@ -0,0 +1,17 @@
+package org.example.lld.casestudies.parkinglot;
+
+public class ParkingLot {
+}
+
+/*
+
+Classes:
+Lot
+Parking Floor
+Slots
+VehicleType
+Token -> Entry/Exit Gate
+Payment Receipt
+Gates
+Price Calculation
+*/
